82c5d05605bb8f64717609c43cf3dff035952ec7
jsatuit <jsatuit@users.noreply.github.com>
2024-04-09 09:02:47 +0200

    End tlan parsing with 'REP' command

--- src/tarlan.py
+++ src/tarlan.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# from collections import OrderedDict
+
 
 class TarlanError(Exception):
     """
@@ -10,6 +12,8 @@ class TarlanError(Exception):
             super().__init__("The .tlan file has errors in line", line_number, ":", msg)
         else:
             super().__init__("The TARLAN command has errors:", msg)
+
+
 
 
 class Tarlan():
@@ -70,66 +74,61 @@ class Tarlan():
         else:
             raise TarlanError("RF output is off!", self.reading_line)
     
-    def parse_args(self, args: list[str]) -> None:
-        """
-        Parses TARLAN arguments by running the function in the Tarlan class with the same name.
+    
+def parse_line(line: str, line_number: int = 0) -> None | tuple[float, list[str]]:
+    time = 0
+    commands = []
+    
+    # Filter away comments
+    codeline = line.split("%")[0]
+    
+    # Unpack arguments
+    args = codeline.split()
+    
+    if len(args) == 0:
+        pass
+    elif args[0] == "AT":
+        # The radar hardware is doing something!
+        if len(args) < 3:
+            raise TarlanError("Line starting with 'AT' must include time and command(s)!", line_number)
         
-        There must be at least two arguments. The first is the time the command is executed, the second atre 
+        time = float(args[1])
         
-        :param args: list of arguments
-        :type args: list[str]
-        :raises TarlanError: If there are too few arguments
-        :rtype: None
-
-        """
-        
-        # Argument must contain at least time and commands
-        if len(args) < 2:
-            raise TarlanError("The line must contain of 'AT', timepoint and command(s), in that order!", self.reading_line)
-        
-                                   
-        time = args[0]
-        cmds = []
-        for arg in args:
+        for arg in args[2:]:
             for cmd in arg.split(","):
-                cmds.append(cmd)
+                commands.append(cmd)
         
-        for cmd in cmds:
-            # «Run» the commands
-            if hasattr(self,cmd):
-                f = getattr(self, cmd)
-                f(time)
         
-    def parse_line(self, line: str):
-        # Filter away comments
-        codeline = line.split("%")[0]
+    elif args[0] == "SETTCR":
+        # Set time control ?
+        time = float(args[1])
         
-        # Unpack arguments
-        args = codeline.split()
-        
-        if len(args) == 0:
-            pass
-        elif args[0] == "AT":
-            # The radar hardware is doing something!
-            self.parse_args(args[1:])
-            
-        elif args[0] == "SETTCR":
-            # Set time control ?
-            if float(args[1]) > 0:
-                # Jump over the rest, now we need only one pulse
-                raise StopIteration
-        else:
-            raise TarlanError("Line must start with 'AT' or 'SRTTCR'. Use '%' for comments.", self.reading_line)
-            
-    def tarlan_parser(self, filename: str = ""):
-        
-        with open(filename) as file:
-            for il, line in enumerate(file):
-                self.reading_line = il + 1
+        commands = ["SETTCR"]
 
-                try:
-                    self.parse_line(line)
-                except StopIteration:
-                    break
-
-            self.reading_line = 0
+    else:
+        raise TarlanError("Line must start with 'AT' or 'SRTTCR'. Use '%' for comments.", line_number)
+        
+    return time, commands
+        
+def tarlan_parser(filename: str = "") -> dict[float,dict[float,list[str]]]:
+    
+    cycle_commands = {}
+    
+    subcycle_start_time = 0.0
+    
+    with open(filename) as file:
+        for il, line in enumerate(file):
+            t, cmds = parse_line(line, il+1)
+            if cmds:
+                if "SETTCR" in cmds:
+                    subcycle_start_time = t
+                    if subcycle_start_time not in cycle_commands.keys():
+                        cycle_commands[subcycle_start_time] = {}
+                    # else:
+                    #     break
+                else:
+                    cur_subcycle = cycle_commands[subcycle_start_time]
+                    cur_subcycle[t] = cmds
+                    if "REP" in cmds:
+                        break
+    return cycle_commands
