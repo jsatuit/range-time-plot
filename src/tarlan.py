01c6dd573146048200a00cf15686bc192bd2ab8f
jsatuit <jsatuit@users.noreply.github.com>
2024-04-12 11:08:45 +0200

    Docs on tarlan import functions

--- src/tarlan.py
+++ src/tarlan.py
@@ -64,21 +64,33 @@ class Subcycle:
         insort_left(self.commands, cmd)
         
 class DataStreams:
+    """
+    Contain intervals for when data streams are enabled. 
+    Intervals may be open in contrast to TimeInterval, which must contain 
+    closed intervals
+    """
+    
     def __init__(self, name: str):
+        """
+        
+        :param name: Name of datastream
+        :type name: str
+
+        """
         self.name = name
         self._streams = []
-        self._on = []
-        self._off = []
     
     @property
-    def state(self):
+    def state(self) -> bool:
+        """
         
-        # if len(self._on) == len(self._off):
-        #     on = False
-        # elif len(self._on) == len(self._off) + 1:
-        #     on = True
-        # else:
-        #     raise RuntimeError(f"Data stream {self.name} is turned on {len(self.on)} times and off {len(self.off)} times. This is not allowed!")
+        :raises RuntimeError: if the last stream contains more than one on and 
+            off value(should never happen, but who knows...)
+        :return: state (on/off) of data stream.
+        :rtype: bool
+
+        """
+        
         if len(self._streams) == 0:
             on = False
         elif len(self._streams[-1]) == 0:
@@ -94,49 +106,99 @@ class DataStreams:
     
     @property
     def is_off(self) -> bool:
+        """
+        If stream is turned off
+        
+        :type: bool
+
+        """
         return not self.state
     
     @property
     def is_on(self) -> bool:
+        """
+        If stream is turned on
+        
+        :type: bool
+
+        """
         return self.state
     
-    def turn_on(self, time: float, line: int):
+    def turn_on(self, time: float, line: int) -> None:
+        """
+        Turns on data stream
+        
+        :param time: time at which to turn on the stream
+        :type time: float
+        :param line: line in the tlan file. Used for error handling only.
+        :type line: int
+        :raises TarlanError: if stream is on already
+
+        """
         if self.is_off:
             self._streams.append([time])
-        # if len(self._on) == len(self._off):
-        #     self._on.append(time)
         else:
             raise TarlanError(f"Data stream {self.name} is already on!", line)
     
     def turn_off(self, time: float, line: int):
+        """
+        Turns off data stream
+        
+        :param time: time at which to turn off the stream
+        :type time: float
+        :param line: line in the tlan file. Used for error handling only.
+        :type line: int
+        :raises TarlanError: if stream is off already
+
+        """
         if self.is_on:
             self._streams[-1].append(time)
-        # if len(self._on) - 1 == len(self._off):
-        #     self._off.append(time)
         else:
             raise TarlanError(f"Data stream {self.name} is already off!", line)
             
     @property
     def nstreams(self) -> int:
-        # return len(self._on)
+        """
+        Number of streams
+        
+        :type: int
+
+        """
         return len(self._streams)
     
     def __len__(self) -> int:
-        # return len(self._on)
+        """
+        Number of streams
+        
+        :type: int
+
+        """
         return self.nstreams
     
     @property
     def intervals(self) -> list[TimeInterval]:
+        """
+        Return the on-time of the streams as a list of TimeIntervals
+        
+        :type: list[TimeInterval]
+
+        """
         if self.is_on:
             raise RuntimeError("Stream is on. Cant return open intervals.")
         iv = []
         for i in range(self.nstreams):
-            # iv.append(TimeInterval(self._on[i], self._off[i]))
             iv.append(TimeInterval(*self._streams[i]))
         return iv
 
     @property
-    def last_turn_off(self):
+    def last_turn_off(self) -> float:
+        """
+        Last time when the stream was turned off
+        
+        :raises RuntimeError: If there stream has not been turned off yet.
+        :type: float
+
+        """
         if self.nstreams == 0:
             raise RuntimeError("Stream has not been turned on yet!")
         elif self.is_off:
@@ -162,6 +224,14 @@ class Tarlan():
     """
     
     def __init__(self, file_name: str = ""):
+        """
+        Starts reading tlan file
+        
+        :param file_name: Path of tlan file to load. No file is loaded if 
+            string is empty, defaults to ""
+        :type file_name: str, optional
+
+        """
         
         streams = ["RF", "CYCLE", "SUBCYCLE"]
         for i in range(1,7):
@@ -178,7 +248,7 @@ class Tarlan():
         for i in range(1, 7):
             setattr(self, f"_CH{i}", [])
             
-            #Time control
+        # Time control
         self.TCR = 0
         self.end_time = 0
         
@@ -187,6 +257,16 @@ class Tarlan():
             
 
     def to_exp(self) -> Experiment:
+        """
+        Convert loaded tarlan input to Experiment
+        
+        :raises RuntimeError: If radar controller is not resetted correctly 
+            at end of file.
+        :raises RuntimeWarning: if transmitter is constantly off.
+        :return: object containing experiment information and plotting functions.
+        :rtype: Experiment
+
+        """
         exp = Experiment()
         exp.add_stop_time(self.streams["CYCLE"].last_turn_off)
         
@@ -215,7 +295,14 @@ class Tarlan():
     
     
         
-    def from_tlan(self, file_name: str = ""):
+    def from_tlan(self, file_name: str = "") -> None:
+        """
+        Parse tlan file and run Tarlan.eec_cmd() for all commands.
+        
+        :param file_name: Path of tlan file to load, defaults to ""
+        :type file_name: str, optional
+
+        """
         cycle = tarlan_parser(file_name)
         self.streams["CYCLE"].turn_on(0, 0)
         for subcycle in cycle:
@@ -224,6 +311,15 @@ class Tarlan():
                 self.exec_cmd(cmd, 0)
     
     def exec_cmd(self, cmd: Command, line: int):
+        """
+        «Execute» TARLAN command / import command to experiment
+        
+        :param cmd: command as pasrsed from the file.
+        :type cmd: Command
+        :param line: line of where the command is in the file.
+        :type line: int
+
+        """
         
         if cmd.cmd == "RFON":
             self.streams["RF"].turn_on(self.TCR + cmd.t, line)
@@ -295,7 +391,18 @@ def parse_line(line: str, line_number: int = 0) -> list[Command]:
         
     return commands
         
-def tarlan_parser(filename: str = "") -> list[Subcycle]:
+def tarlan_parser(filename: str) -> list[Subcycle]:
+    """
+    Parse tlan file by running parse_line for every line of code. Commands are
+        grouped in Subcycle objects
+    
+    :param filename: Path to tlan file, defaults to ""
+    :type filename: str
+    :raises FileNotFoundError: when file is not found / does not exist
+    :return: commands grouped in Sybcycles.
+    :rtype: list[Subcycle]
+
+    """
     
     cycle = []
     
