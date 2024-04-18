b6c287a01675c2d48e1d1be1bf594a247921fad1
jsatuit <jsatuit@users.noreply.github.com>
2024-04-18 10:45:20 +0200

    Add some list comparisons

--- src/timeInterval.py
+++ src/timeInterval.py
@@ -90,8 +90,20 @@ class TimeInterval:
         elif other.begin <= self.end and other.end <= self.begin:
             overlap = False
         
-        
         return overlap
+    
+    def overlaps_any(self, other: list[Self]) -> bool:
+        """
+        Checks if TimeInterval overlaps with any other TimeInterval in list
+        
+        :param other: list of TimeInterval objects to check overlap with
+        :type other: list[TimeInterval]
+        :return: True if any intervals overlap, else False
+        :rtype: bool
+
+        """
+        return any([self.overlaps_with(iv) for iv in other])
+        
     def check_overlap(self, other: Self):
         """
         Checks if TimeInterval overlaps with other TimeInterval. If so, 
@@ -109,14 +121,30 @@ class TimeInterval:
         """
         Return True if this interval is within the other interval. 
         
-        Intervals are allowed to share boundaries. Numberical errors are not 
+        Intervals are allowed to share boundaries. Numerical errors are not 
             taken into account
             
-        :param other: Outer TimeInterval
-        :type other: Self
+        :param other: Other TimeInterval
+        :type other: TimeInterval
         :return: whwether this interval is within the other one.
         :rtype: bool
 
         """
-        return other.begin <= self.begin and self.end <= other.end
+        if isinstance(other, TimeInterval):
+            return other.begin <= self.begin and self.end <= other.end
+    
+    def within_any(self, other: list[Self]) -> bool:
+        """
+        Return True if this interval is completely within any of the intervals
+        in the list. 
         
+        Intervals are allowed to share boundaries. Numerical errors are not 
+            taken into account
+            
+        :param other: List of TimeInterval objects
+        :type other: list[TimeInterval]
+        :return: whwether this interval is within any of those in the list.
+        :rtype: bool
+
+        """
+        return any([self.within(iv) for iv in other])
