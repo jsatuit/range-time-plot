85269f87ee5d40e73a2159a85d1d3dd918807221
jsatuit <jsatuit@users.noreply.github.com>
2024-04-23 15:25:47 +0200

    Command line: If subcycle 0 is given, plot whole cycle

--- src/main.py
+++ src/main.py
@@ -10,15 +10,29 @@ from src.experiment import Experiment
 @click.argument("path", 
               type = click.Path(exists=True, dir_okay=False, readable=True, resolve_path=True),
               required = True, 
-              default = 9
+              default = 9,
 )
 @click.argument("subcycle",
                 type = int,
                 required = False,
                 default = 1,
                 )
-def main(path, subcycle):
+def main(path: str, subcycle: int):
+    """
+    Tarlan file plotter. 
+    
+    Plots EISCAT KST experiment specified with a .tlan file
+    
+    \f
+    
+    :param str path: Path to tlan file which to plot
+    :param int subcycle: Subcycle to plot. Zero means plot all.
+
+    """
     print(f"Loading and plotting experiment {path}")
-    Experiment.from_eiscat_kst(path).subcycles[subcycle-1].plot()
+    if subcycle == 0:
+        Experiment.from_eiscat_kst(path).plot()
+    else:
+        Experiment.from_eiscat_kst(path).subcycles[subcycle-1].plot()
     plt.show()
 
