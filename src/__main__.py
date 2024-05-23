b8821fd1e3c5b6e0bc39317570a5e04a2727f87d
jsatuit <jsatuit@users.noreply.github.com>
2024-05-23 16:44:49 +0200

    switch to argparse

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import argparse
 
 from src.main import main
 
-main()
+
+parser = argparse.ArgumentParser(description="Plot EISCAT experiments")
+parser.add_argument("path", type=str)
+parser.add_argument("subcycle", type=int, nargs = "?", default = 1)
+args = parser.parse_args()
+
+main(args.path, args.subcycle)
--- src/main.py
+++ src/main.py
@@ -1,30 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import click
 import matplotlib.pyplot as plt
 
 from src.experiment import Experiment
 
-@click.command()
-@click.argument("path", 
-              type = click.Path(exists=True, dir_okay=False, readable=True, resolve_path=True),
-              required = True, 
-              default = 9,
-)
-@click.argument("subcycle",
-                type = int,
-                required = False,
-                default = 1,
-                )
+
 def main(path: str, subcycle: int):
     """
     Tarlan file plotter. 
-    
+
     Plots EISCAT KST experiment specified with a .tlan file
-    
+
     \f
-    
+
     :param str path: Path to tlan file which to plot
     :param int subcycle: Subcycle to plot. Zero means plot all.
 
@@ -35,4 +24,3 @@ def main(path: str, subcycle: int):
     else:
         Experiment.from_eiscat_kst(path).subcycles[subcycle-1].plot()
     plt.show()
-
