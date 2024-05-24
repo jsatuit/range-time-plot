681f340e89c79b040ee5de7b2cc4fc27f0b81e4f
jsatuit <jsatuit@users.noreply.github.com>
2024-05-24 05:54:46 +0200

    correct + docstring+autopep8

--- src/tlan/tarlan.py
+++ src/tlan/tarlan.py
@@ -26,22 +26,20 @@ from src.eventlist import EventList
 from src.const import km, µs, c
 
 tarlan_command_docstring =\
-""":param time: time [s]
+    """:param time: time [s]
 :type time: float
 :param line: line where the command is found. Used for error handling only.
 :type line: int"""
 """Docstring ito be inserted to all tarlan commands."""
 
 
-
 def kst_channels():
     "List of available channels"
-    
-    chs = []
-    for i in range(1,7):
-        chs.append(f"CH{i}") 
-    return chs
 
+    chs = []
+    for i in range(1, 7):
+        chs.append(f"CH{i}")
+    return chs
 
 
 def do_nothing(*args):
@@ -50,19 +48,20 @@ def do_nothing(*args):
        implement the function here."""
     pass
 
+
 class Command:
     """
     Tarlan command.
-    
+
     :param float t: Subcycle time in seconds when command is executed.
     :param str cmd: Command as written in the .tlan file
     :param int line: Line in tlan file. Used for error messages
-    
+
     """
-    
+
     def __init__(self, t: float, cmd: str, line: int = 0):
         """
-        
+
         :param t: Subcycle time in seconds when command is executed.
         :type t: float
         :param cmd: Command as written in the .tlan file
@@ -74,24 +73,22 @@ class Command:
         self.t = t
         self.cmd = cmd
         self.line = line
-        
+
     def __lt__(self, other: Self) -> bool:
         # Is used for sorting. Therefore only the time matters
         return self.t < other.t
-    
+
     def __repr__(self):
         return f'Command({self.t}, "{self.cmd}", {self.line})'
-    
+
     def __str__(self):
         return f"{self.line}: {self.t/µs} {self.cmd}"
-    
 
-        
-    
+
 class Tarlan():
     """
     Class for parsing and handling an TARLAN experiment
-    
+
     :param IntervalList cycle: Begin and end of experiment cycle
     :param IntervalList subycle: Begin and end of experiment subcycles. These
         are defined by when SETTCR <time> is called.
@@ -100,11 +97,10 @@ class Tarlan():
         "RF", "CH1".
     :param float end_time: Length of tarlan program in seconds.
     """
-    
-        
+
     command_docs = {
-        "CHQPULS": "High output on bit 31 for 2 us, used for synchronization "+
-            "with external hardware.",
+        "CHQPULS": "High output on bit 31 for 2 us, used for synchronization " +
+        "with external hardware.",
         "RXPROT": "Enable receiver protector, bit 12 high",
         "RXPOFF": "Disable receiver protector, bit 12 low",
         "LOPROT": "Enable local oscillator protector, bit 6 high",
@@ -119,16 +115,16 @@ class Tarlan():
         "CAL100": "Enable noise source for calibration, bit 15 high.",
         "CALOFF": "Tromsø and receivers: Disable noise source, bit 15 low.",
         "CAL0": "Disable noise source for calibration, bit 15 low.",
-        "HCALON": "Remote receivers: enable noise source in only horisontal "+
-            "wave guide, high bit 1 high",
-        "HCALOFF": "Remote receivers: disable noise source in only horisontal "+
-            "wave guide, high bit 1 low",
-        "VCALON": "Remote receivers: enable noise source in only vertical "+
-            "wave guide, high bit 1 high",
-        "VCALOFF": "Remote receivers: disable noise source in only vertical "+
-            "wave guide, high bit 1 low",
-        "STC": "Send a interrupt to crate computer to signal that new data are "+
-            "available a needs to be taken care of, bit 8 high strobed.",
+        "HCALON": "Remote receivers: enable noise source in only horisontal " +
+        "wave guide, high bit 1 high",
+        "HCALOFF": "Remote receivers: disable noise source in only horisontal " +
+        "wave guide, high bit 1 low",
+        "VCALON": "Remote receivers: enable noise source in only vertical " +
+        "wave guide, high bit 1 high",
+        "VCALOFF": "Remote receivers: disable noise source in only vertical " +
+        "wave guide, high bit 1 low",
+        "STC": "Send a interrupt to crate computer to signal that new data are " +
+        "available a needs to be taken care of, bit 8 high strobed.",
         "BUFLIP": "Change side of buffer memory in channel boards, bit 17 high strobed.",
         "ALLOFF": "Close sampling gate on all channel boards, bit 10-15 low",
         "REP": "End of tarlan program/repeat cycle",
@@ -139,15 +135,15 @@ class Tarlan():
         "AD1R": "Route input for AD 1 to channel board 4, 5, 6.",
         "AD2L": "Route input for AD 2 to channel board 1, 2, 3.",
         "AD2R": "Route input for AD 2 to channel board 4, 5, 6.",
-        "STFIR": "Start the fir filters onboard channel boards, necessary "+\
-            "to do before using them, bit 16 strobed.",
+        "STFIR": "Start the fir filters onboard channel boards, necessary " +
+        "to do before using them, bit 16 strobed.",
         "TRANS": "Not documented.",
         "RECEV": "Not documented.",
-        }
+    }
     for i in range(16):
         command_docs["F" + str(i)] = "Set transmitter frequency, bit 0-3 high"
     for i in range(1024):
-        command_docs["NCOSEL" + str(i)] = "Load the frequency defined in the "+\
+        command_docs["NCOSEL" + str(i)] = "Load the frequency defined in the " +\
             "requested memory into the NCO plus strobe bit 29."
     for ch in kst_channels():
         command_docs[ch] = \
@@ -155,29 +151,28 @@ class Tarlan():
         command_docs[ch + "OFF"] = \
             f"Close sampling gate on the referenced channel board, bit {i+9} low"
     for i in range(32):
-        command_docs["BRX" + str(i)] = f"Set bit {i} on receiver controller. "+\
+        command_docs["BRX" + str(i)] = f"Set bit {i} on receiver controller. " +\
             "No checks are made."
-        command_docs["BRX" + str(i) + "OFF"] = f"Set bit {i} on receiver controller. "+\
+        command_docs["BRX" + str(i) + "OFF"] = f"Set bit {i} on receiver controller. " +\
             "No checks are made. "
-        command_docs["BTX" + str(i)] = f"Set bit {i} on transmitter controller. "+\
+        command_docs["BTX" + str(i)] = f"Set bit {i} on transmitter controller. " +\
             "No checks are made. Use with caution."
-        command_docs["BTX" + str(i) + "OFF"] = f"Set bit {i} on transmitter controller. "+\
+        command_docs["BTX" + str(i) + "OFF"] = f"Set bit {i} on transmitter controller. " +\
             "No checks are made. Use with caution."
-        
+
     def __init__(self, filename: str = ""):
         """
         Initializing Tarlan(). If .tlan file is specified, it will be loaded.
-        
-        :param file_name: Path of tlan file to load. No file is loaded if 
+
+        :param str, optional filename: Path of tlan file to load. No file is loaded if 
             string is empty, defaults to ""
-        :type file_name: str, optional
 
         """
         self.cycle = IntervalList("CYCLE")
         # self.subcycle_list = IntervalList("SUBCYCLE")
         self.subcycle_list = TarlanSubcycle()
         self.phaseshifter = PhaseShifter()
-        
+
         self.stream_names = ["RF", "RXPROT", "LOPROT", "CAL", "BEAM"]
         for ch in kst_channels():
             self.stream_names.append(ch)
@@ -185,51 +180,50 @@ class Tarlan():
         self._generate_commands()
         self._check_command_docs()
         self._loaded_FIR = False
-            
+
         # Time control
         self.TCR = 0
         self.end_time = 0
-        
+
         if filename:
             self.from_tlan(filename)
-    
+
     def _init_streams(self):
         # Create streams
         self.streams = dict()
         for stream in self.stream_names:
             self.streams[stream] = IntervalList(stream)
 
-                    
     def from_tlan(self, filename: str = "") -> None:
         """
         Parse tlan file and run Tarlan.exec_cmd() for all commands.
-        
+
         :param filename: Path of tlan file to load, defaults to ""
         :type filename: str, optional
-        
+
         """
         cmd_list = tarlan_parser(filename)
-        
+
         for cmd in cmd_list:
-            
+
             # First subcycle
             if self.cycle.is_off:
                 self.cycle.turn_on(0, cmd.line)
                 self.subcycle_list.turn_on(0, cmd.line)
-                    
+
             """ Use SETTCR as start and stop of subcycles.
             In this implementation, SETTCR 0 means that subcycle is continued."""
             if cmd.cmd == "SETTCR":
-                
+
                 if cmd.t > 0:
                     self.subcycle_list.turn_off(cmd.t, cmd.line, self.streams)
-                    
+
                     # Delete connection to last subcycle streams
                     self._init_streams()
-                    
+
                     self.subcycle_list.turn_on(cmd.t, cmd.line)
                 self.SETTCR(cmd.t, cmd.line)
-                
+
             elif cmd.cmd == "REP":
                 # self.PHA_OFF(cmd.t, cmd.line)
                 self.subcycle_list.turn_off(cmd.t, cmd.line, self.streams)
@@ -239,39 +233,37 @@ class Tarlan():
             else:
                 self.exec_cmd(cmd)
         self.filename = filename
-        
+
     def ALLOFF(self, time: float, line: int):
         f"""
         Turn off signal reception with all channels
-        
+
         {tarlan_command_docstring}
 
         """
-        for i in range(1,7):
+        for i in range(1, 7):
             CH = "CH"+str(i)
             if self.streams[CH].is_on:
                 self.streams[CH].turn_off(time, line)
-                
+
     def STFIR(self, time: float, line: int):
         if self._loaded_FIR > 0:
-            warn(f"STFIR was called on line {line}, but FIR filters are "+\
-                  "loaded already!", TarlanWarning)
+            warn(f"STFIR was called on line {line}, but FIR filters are " +
+                 "loaded already!", TarlanWarning)
         else:
             self._loaded_FIR = time
-            
+
     def SETTCR(self, time: float, line: int):
         "Set reference time in time control"
         self.TCR = time
-        
-        
+
     def _generate_commands(self):
-        #"""Implement other TARLAN commands.
+        # """Implement other TARLAN commands.
         #
-        #These are:
-        #    
-        #"""
-        
-        
+        # These are:
+        #
+        # """
+
         #### Implement TARLAN commands from here ####
         commands = {
             "TRANS": do_nothing,  # No documentation
@@ -302,12 +294,12 @@ class Tarlan():
             "SETTCR": do_nothing,  # Is not handeled here!
             "BUFLIP": do_nothing,  # Too technical here
             "STC": do_nothing,  # Too technical here
-            }
+        }
         # Add receiver channel commands
         for ch in kst_channels():
             commands[ch] = self.streams[ch].turn_on
             commands[ch+"OFF"] = self.streams[ch].turn_off
-            
+
         # Handling Frequencies is not yet implemented
         # TODO: handle frequencies
         for f in range(16):
@@ -317,7 +309,7 @@ class Tarlan():
         for fline in range(1024):
             sl = f"NCOSEL{fline}"
             commands[sl] = do_nothing
-        
+
         ''' 
         Silent warnings on setting single bits 4 or 5 in transmit and receive 
         controllers. These go to ADC samplegate, but are not of interest here.
@@ -326,57 +318,54 @@ class Tarlan():
             for d in ["R", "T"]:
                 commands[f"B{d}X{i}"] = do_nothing
                 commands[f"B{d}X{i}OFF"] = do_nothing
-        #### Implement TARLAN commands to here ####  
+        #### Implement TARLAN commands to here ####
         self.commands = commands
-        
+
     def _check_command_docs(self):
         # Check that all commands have docstring
         for cmd in self.commands.keys():
             if cmd not in self.command_docs.keys():
                 print("Command", cmd, "has no docstring!")
-        
-        
+
     def exec_cmd(self, cmd: Command):
         """
         «Execute» TARLAN command / import command to experiment
-        
+
         :param cmd: command as pasrsed from the file.
         :type cmd: Command
 
         """
         if self.cycle.is_off:
             raise TarlanError("The cycle has not been started!", cmd.line)
-        
+
         if self.subcycle_list.is_off:
             raise TarlanError("No subcycle has been started yet!", cmd.line)
-            
-        # Has to be done again and again because python does some strange 
+
+        # Has to be done again and again because python does some strange
         # function saving?
         self._generate_commands()
-            
+
         if cmd.cmd in self.commands.keys():
             # print(self.TCR, cmd.t)
             self.commands[cmd.cmd](self.TCR + cmd.t, cmd.line)
         else:
-            warn(f"Command {cmd.cmd}, called from line {cmd.line} "+\
-                  "is not implemented yet", TarlanWarning)
+            warn(f"Command {cmd.cmd}, called from line {cmd.line} " +
+                 "is not implemented yet", TarlanWarning)
 
-    
     def phaseshifts(self, idx: int = 0) -> tuple[EventList, list[float]]:
-        
+
         # Does this subcycle exist (or has it been parsed yet?)
-        
+
         sc_interval = self.subcycle_list.intervals[idx]
         tx_intervals = self.subcycle_list.data_intervals[idx]["RF"].intervals
-        
+
         return self.phaseshifter.phase_shifts_within(sc_interval, tx_intervals)
-        
-    
-        
+
+
 def parse_line(line: str, line_number: int = 0) -> list[Command]:
     """
     Parse single line of Tarlan code.
-    
+
     :param line: line of code
     :type line: str
     :param line_number: line number in file, used for error handling only, defaults to 0
@@ -388,44 +377,45 @@ def parse_line(line: str, line_number: int = 0) -> list[Command]:
     """
     time = 0
     commands = []
-    
+
     # Filter away comments
     codeline = line.split("%")[0]
-    
+
     # Unpack arguments
     args = codeline.split()
-    
+
     if len(args) == 0:
         pass
     elif args[0] == "AT":
         # The radar hardware is doing something!
         if len(args) < 3:
-            raise TarlanError("Line starting with 'AT' must include time and command(s)!", line_number)
-        
+            raise TarlanError(
+                "Line starting with 'AT' must include time and command(s)!", line_number)
+
         time = float(args[1])*µs
-        
+
         for arg in args[2:]:
             for cmd in arg.split(","):
                 commands.append(Command(time, cmd, line_number))
-        
-        
+
     elif args[0] == "SETTCR":
         # Set time control ?
         time = float(args[1])*µs
-        
+
         commands = [Command(time, "SETTCR", line_number)]
 
     else:
-        raise TarlanError("Line must start with 'AT' or 'SETTCR'. Use '%' for comments.", line_number)
-        
+        raise TarlanError(
+            "Line must start with 'AT' or 'SETTCR'. Use '%' for comments.", line_number)
+
     return commands
-        
+
 
 def tarlan_parser(filename: str = "") -> list[Command]:
     """
     Parse tlan file. Commands are returned in the same order as they appear in 
     the file.
-    
+
     :param filename: filename, defaults to ""
     :type filename: str
     :return: list of Command objects
@@ -441,4 +431,3 @@ def tarlan_parser(filename: str = "") -> list[Command]:
                 if cmd.cmd == "REP":
                     break
     return cmd_list
-
