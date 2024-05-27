6b60152520546aeffc0703b51a088a9e8331662a
jsatuit <jsatuit@users.noreply.github.com>
2024-05-27 07:48:22 +0200

    Numerically controlled oscillator file loading file

---
+++ src/kstconfig/nco.py
@@ -0,0 +1,15 @@
+"""
+Handling .nco files.
+
+At EISCAT, these are loaded by the numerical controlled oscillator. These shift frequency of the signal 
+that comes into a channel. When reading these files, one should be aware of that when the signal comes into te channels, its frequency already is shifted twice.
+
+# EISCAT 
+
+
+# At UHF
+At EISCAT UHF, the incoming signal is 
+
+Visually, this procedure is shown in [Jussis EISCAT portal]
+(https://portal.eiscat.se/jussi/eiscat/erosdoc/uhf_radar.html)
+"""
