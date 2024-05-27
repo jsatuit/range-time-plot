87f951ccd9275d794765fb07a913d13ace7ae50d
jsatuit <jsatuit@users.noreply.github.com>
2024-05-27 09:53:47 +0200

    test nco parsing

---
+++ test/kstconfig/test_nco.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 27 09:29:41 2024
+
+"""
+import pytest
+
+from src.kstconfig.nco import Nco
+
+def test_reading():
+    # test of valid file (cp1l, ch1)
+    
+    file = """NCOPAR_VS       0.1
+    %======================================
+    % cp1l_ch1
+    % LO1=812.0 MHz LO2=128.0 MHz
+    %======================================
+    NCO	0	 10.4	% f12
+    NCO	1	 10.1	% f13
+    NCO	2	 10.1	% f13
+    NCO	3	 10.4	% f12"""
+    
+    freq = Nco.parse_nco(file)
+    assert freq == [10.4, 10.1, 10.1, 10.4]
+    
+    
+    # Test of bad file - missing first line
+    badfile = """NCO	0	 10.4	% f12
+    NCO	1	 10.1	% f13
+    NCO	2	 10.1	% f13
+    NCO	3	 10.4	% f12"""
+    
+    with pytest.raises(AssertionError):
+        Nco.parse_nco(badfile)
+    
+    # Test of bad file - wrong first line
+    faultyfile = """NCOPAR_VS       0.2
+    %======================================
+    % cp1l_ch1
+    % LO1=812.0 MHz LO2=128.0 MHz
+    %======================================
+    NCO	0	 10.4	% f12
+    NCO	1	 10.1	% f13
+    NCO	2	 10.1	% f13
+    NCO	3	 10.4	% f12"""
+    
+    with pytest.raises(AssertionError):
+        Nco.parse_nco(faultyfile)
+    
